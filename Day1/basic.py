1046cf0d8c8999984ca8dfce0b07a5dad433662c
Madhuri <madhurirajbanshi@gmail.com>
2023-08-09 11:00:06 +0545

    workshop

---
+++ Day1/basic.py
@@ -0,0 +1,96 @@
+# b=int(input("enter your weight "))
+# a=input("Do you want to change in pound(p) or (kg)").lower()
+# if(a=='k'):
+#     print("weight in kg=", b/2.2)
+# elif(a=='p'):
+#     print("weight in pound=",b*2.2)
+
+#Range
+# a=[]
+# for i in range(0,6):
+#     a.append(i)
+# print(a)
+# print(min(a))
+# if(i==1)
+
+# a=[]
+# for i in range(0,len([1,3,5,7,9])):
+#     a.append(i)
+# print(a)
+# a=[]
+# for i in range(0,20):
+#     if(i%2==1):
+#         a.append(i)
+# print(a)
+array=[1,2,3,4,"hello"]
+print(array)
+arrayofNames =["Cat","Dog"]
+for i,name in enumerate(arrayofNames):
+    print({i:name})
+
+
+# def dividethree(num):
+#     return num%3== 0
+# result = list(filter(dividethree,a))
+# print(result)
+# def squarethree(num):
+#     return num*num
+# result =list (map(squarethree ,result))
+# print(result)
+# first=[1,2,3]
+# second=[4,5,6]
+# for f,s in zip(first,second):
+#     print(f,s)
+# def sum(a , b):
+#     return a+b
+# result = sum(1,2)
+# print(result)
+# #tuple
+# tuplesExample =(1, "Hello","world",True)
+# #print(tuplesexample[0])
+# tuplesExample[0]=2
+# print(tuplesExample)
+
+# def odd(num):
+#     return num%2== 1
+# result = tuple(filter(odd,range(0,20)))
+# print(result)
+# i= 1
+# while i<=5:
+#     print(i * '!')
+#     i=i+1
+# course="Python for"
+# print(course.replace('n','5'))
+# q=[1,2,3,4]
+# print(1 in q)
+# numbers=[1,2,3,4]
+# print(len(numbers))
+# numbers=[1,2,3,4]
+# for item in numbers:
+#     print(item)
+# i=0
+# while i< len(numbers):
+#     print(numbers[i])
+#     i=i+1
+
+# numbers=range(5)
+# for number in numbers:
+#     print(number)
+
+
+
+#Day 2
+# Supervised learning
+# UnSupervised learning
+# Reinforcement learning
+# Dataset=> csv file
+
+
+a=input("Enter your name ? ")
+print("Hello " + a)
+print("Hi Computer ")
+
+
+
+
+    
